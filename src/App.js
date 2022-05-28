a6cbf9dd1bfac1a23f2e3f30f0596fdc3c3bdaaf
unbugster <unbugster@gmail.com>
2022-05-28 12:50:33 +0300

    Add the initial data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Timer } from './Timer.js';
+import { React } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="header__title">Pomodoro-timer</h1>
       </header>
+      <main className="main">
+        <Timer mins="25" secs="00" divider=":" />
+      </main>
     </div>
   );
 }
 
 export default App;
+
